be9740e0aad1e82065c899cc577673c2551a00f3
David <dave.pylypenko@gmail.com>
2022-05-18 11:21:08 +0300

    Update Shortest_Word.js
    Сделал решение О(n) для задачи

--- homework_1/Shortest_Word.js
+++ homework_1/Shortest_Word.js
@@ -1,5 +1,19 @@
+// First solution
 function findShort(s){
     let arr = s.split(' ');
     arr.sort( (a, b) => a.length - b.length)
     return arr[0].length
   }
+
+
+// Second solution
+function findShort(s){
+  s = s.split(' ')
+  let result = 100;
+  for (let i of s) {
+    if (i.length < result) {
+      result = i.length
+    }
+  }
+  return result
+}
