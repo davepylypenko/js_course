7410b93c07b73d7357be9f56c3b9e1dd3f8b242d
David <dave.pylypenko@gmail.com>
2022-05-24 14:43:33 +0300

    Update Add_property.js

--- homework_2/Add_property.js
+++ homework_2/Add_property.js
@@ -1,3 +1,4 @@
+// первое решение
 function addUserAnswer(arr) {
   for (let i = 0; i < arr.length; i++) {
     arr[i].usersAnswer = null
@@ -5,4 +6,10 @@ function addUserAnswer(arr) {
   return arr
 }
 
+// второе решение (forEach)
+function addUserAnswer(arr) {
+  arr.forEach( a => a.usersAnswer = null )
+  return arr
+}  
+  
 addUserAnswer(questions)
